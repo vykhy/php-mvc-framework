15bdde7aacc8e52261fa35ec4b1332aa264938a2
vikehieto <vikehietoyhoshii@gmail.com>
2021-09-15 18:18:48 +0530

    implemented initial migration

--- core/Database.php
+++ core/Database.php
@@ -14,5 +14,30 @@ class Database
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $this->getAppliedMigrations();
+
+        $files = scandir(Application::$ROOT_DIR . '/migrations');
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                migration VARCHAR(255),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            ) ENGINE=INNODB;");
+    }
+
+    public function getAppliedMigrations()
+    {
+        $stmt = $this->pdo->prepare("SELECT migration FROM migrations");
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_COLUMN);
+    }
 }
 ?>
