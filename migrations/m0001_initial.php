f6870e5c8c6a2833d2ebc0f555e2e482048321dc
vikehieto <vikehietoyhoshii@gmail.com>
2021-09-15 17:51:30 +0530

    implemented dotenv and db conn

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\migrations;
+
+class m0001_initial
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        
+    }
+}
+?>
---
+++ migrations/m0002_something.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\migrations;
+
+class m0001_something
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        
+    }
+}
+?>
